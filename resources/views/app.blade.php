4b6a7c9dc9e7bb4b8132ef299e86a1a4dce2dd93
Kalen Jordan <kalen@magemail.co>
2019-01-10 07:10:07 -0600

    Card layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,8 +14,27 @@
 <body>
     <div class="wrapper">
         <div class="card">
-            <img src="https://pbs.twimg.com/profile_images/959539398210547712/U5lQBX2N_400x400.jpg">
-            Kalen Jordan
+            <div class="card--background"></div>
+            <div class="card--inner">
+                <div class="card--avatar">
+                    <img src="https://pbs.twimg.com/profile_images/959539398210547712/U5lQBX2N_400x400.jpg">
+                </div>
+                <div class="card--identity mb-1">
+                    <div class="card--identity--name bold">
+                        Kalen Jordan
+                    </div>
+                    <div class="card--identity--handle font-small">@kalenjordan</div>
+                </div>
+                <div class="card--about font-small mb-1">
+                    Magento fanboy. Small-time entrepreneur. Wannabe Youtuber. Not a recruiter. Founder @commercehero. Advisor @getmagemail. Co-host @magetalk
+                </div>
+                <div class="card--tags font-small">
+                    <div class="tag">Magento</div>
+                    <div class="tag">SaaS</div>
+                    <div class="tag">Seeking Advisees</div>
+                    <div class="tag">Developer</div>
+                </div>
+            </div>
         </div>
     </div>
 </body>
