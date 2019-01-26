623d162e8547417069e11742ad8139b4acff780d
Kalen Jordan <kalen@magemail.co>
2019-01-26 16:23:06 -0600

    Add present dot to saved search card

--- resources/js/components/SavedSearchCard.vue
+++ resources/js/components/SavedSearchCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card hoverable">
+    <div class="card saved-search-card hoverable">
         <div class="card--background bg-secondary">
             <h3 class="text-center">
                 <router-link v-if="savedSearch.slug" class="naked-link" :to="{name: 'saved-search', params: {slug: savedSearch.slug}}">
@@ -23,9 +23,10 @@
                 <div class="saved-search--user flex-1" v-for="user in savedSearch.users.slice(0, 6)" v-bind:user="user" :key="user.id">
                     <router-link class="no-link" :to="{ name: 'profile', params: { username: user.username }}">
                         <div class="mini-card m-2 p-3 border border-gray-lighter hover:border-gray-light text-center">
-                            <div>
+                            <div class="user-avatar inline-block relative">
                                 <img class="w-12 h-12 rounded-full border-2 border-secondary-light"
                                      v-bind:src="user.avatar_path">
+                                <i v-if="isPresent(user)" class="absolute is-present fas fa-circle"></i>
                             </div>
                             <div>
                                 <div class="text-sm mb-1">
@@ -46,5 +47,23 @@
 <script>
     export default {
         props: ['savedSearch'],
+        methods: {
+            isPresent(user) {
+                let ids = [];
+                let presentUsers = this.presentUsers;
+                for (let i in presentUsers) {
+                    ids.push(presentUsers[i].id);
+                }
+                return ids.includes(user.id);
+            }
+        },
+        computed: {
+            loggedInUser() {
+                return this.$store.state.user;
+            },
+            presentUsers() {
+                return this.$store.state.presentUsers;
+            },
+        },
     }
 </script>
