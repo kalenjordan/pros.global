62ae6c327b9f8002e0a016a5b0938675eeed33a8
Kalen Jordan <kalen@magemail.co>
2019-01-29 14:57:57 -0600

    Favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -22,6 +22,7 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+    <link rel="shortcut icon" type="image/png" href="/favicon-{{ env('APP_ENV') }}.ico"/>
 
     <script src="https://rawgit.com/showdownjs/showdown/develop/dist/showdown.min.js"></script>
 
