da7d6eeb61608ea9512bc82a122c55425ab56024
Kalen Jordan <kalen@magemail.co>
2019-02-08 11:30:11 -0600

    Unindennt email

--- resources/views/emails/unread-notifications.blade.php
+++ resources/views/emails/unread-notifications.blade.php
@@ -3,16 +3,16 @@
 /** @var \Illuminate\Notifications\DatabaseNotification $notification */
 ?>
 @component('mail::message')
-    # You have {{ $user->notificationsToEmail()->count() }} unread notifications
+# You have {{ $user->notificationsToEmail()->count() }} unread notifications
 
-    @foreach ($user->notificationsToEmail()->orderby('created_at', 'desc')->get() as $notification)
-    - {{ $notification->data['text'] }} - {{ \App\Date::parse($notification->created_at)->diffForHumans() }}
-    @endforeach
+@foreach ($user->notificationsToEmail()->orderby('created_at', 'desc')->get() as $notification)
+- {{ $notification->data['text'] }} - {{ \App\Date::parse($notification->created_at)->diffForHumans() }}
+@endforeach
 
-    @component('mail::button', ['url' => $user->url()])
-        Go to pros.global
-    @endcomponent
+@component('mail::button', ['url' => $user->url()])
+    Go to pros.global
+@endcomponent
 
-    Thanks,
-    pros.global
+Thanks,
+pros.global
 @endcomponent
