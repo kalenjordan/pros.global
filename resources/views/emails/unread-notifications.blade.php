3d1910f6527d2faf0f2a20cf6c42d786d6bdadce
Kalen Jordan <kalen@magemail.co>
2019-02-09 10:32:09 -0600

    Singular versin of notification

--- resources/views/emails/unread-notifications.blade.php
+++ resources/views/emails/unread-notifications.blade.php
@@ -1,9 +1,10 @@
 <?php
 /** @var \App\User $user */
 /** @var \Illuminate\Notifications\DatabaseNotification $notification */
+$count = $user->notificationsToEmail()->count();
 ?>
 @component('mail::message')
-# You have {{ $user->notificationsToEmail()->count() }} unread notifications
+# You have {{ $count }} unread notification{{ $count > 1 ? "s" : "" }}
 
 @foreach ($user->notificationsToEmail()->orderby('created_at', 'desc')->get() as $notification)
 - {{ $notification->data['text'] }} - {{ \App\Date::parse($notification->created_at)->diffForHumans() }}
