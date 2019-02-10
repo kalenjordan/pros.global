458eaf889f722f827249d5ae416f3f06db1a260a
Kalen Jordan <kalen@magemail.co>
2019-02-10 12:00:13 -0600

    Bump the notification email frequency to hourly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('notifications:send --limit=3 --live')
-            ->twiceDaily()
+            ->hourly()
             ->sendOutputTo(storage_path('logs/cron.log'));
     }
 
