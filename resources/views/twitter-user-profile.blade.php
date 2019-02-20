49cfbc35ed3072ec676727347a9a32d8c0001c46
Kalen Jordan <kalen@magemail.co>
2019-02-20 04:28:28 -0600

    Update twitter card for user profile

--- resources/views/twitter-user-profile.blade.php
+++ resources/views/twitter-user-profile.blade.php
@@ -9,29 +9,29 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
 </head>
 <body class="flex items-center">
-    <div class="twitter-card twitter-card-user-profile relative bg-white p-8 mx-auto text-center flex items-center" >
-        <img class="logo w-3rem absolute opacity-50" src="/img/icon-200.png">
+    <div class="twitter-card-bigger twitter-card-user-profile relative mx-auto text-left flex items-center" >
         <div>
-            <div class="avatar-wrapper mb-2">
-                <img class="rounded-full border-4 border-secondary w-35rem" src="{{ $user->avatar_path }}">
+            <div class="image-wrapper relative inline-block">
+                <img class="avatar rounded-full" src="{{ $user->avatar_path }}">
+                <img class="logo absolute" src="/img/icon-200.png">
             </div>
-            <h1 class="font-15rem mb-2">
+            <h1 class="font-15rem mb-4" style="-webkit-box-orient: vertical;">
                 {{ $user->headline }}
             </h1>
-            <div class="tags font-80">
+            <div class="tags font-80" >
                 @foreach ($user->tagged as $tagged)
                     <div class="tag">
                         <span class="tag-name">
                             @if ($tagged->icon)
-                                <i class="{{ $tagged->icon }}"></i>
+                                <i class="tag-icon material-icons">{{ $tagged->icon }}</i>
                             @endif
                             {{ $tagged->tag_name }}
                         </span>
-                        @if ($tagged->upvote_count)
-                            <span class="tag-count">{{ $tagged->upvote_count }}</span>
-                        @endif
                     </div>
                 @endforeach
             </div>
