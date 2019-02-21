e49aec3831af6cc6188634f72f6dd46cbac7a27c
Kalen Jordan <kalen@magemail.co>
2019-02-21 09:38:37 -0600

    Generate api token

--- resources/views/auth/linkedin-callback.blade.php
+++ resources/views/auth/linkedin-callback.blade.php
@@ -2,7 +2,7 @@
 <body>
     <script type="text/javascript">
         @if (env('AUTH_REDIRECT'))
-            window.location='{{ env('AUTH_REDIRECT') }}?api_token={{ Auth::user()->api_token }}';
+            window.location='{{ env('AUTH_REDIRECT') }}?api_token={{ Auth::user()->getOrCreateApiToken() }}';
         @else
             window.opener.Events.$emit('user-authenticated', JSON.stringify({!! Auth::user() ? json_encode(Auth::user()->toArrayForCookie()) : null  !!}));
             window.close();
