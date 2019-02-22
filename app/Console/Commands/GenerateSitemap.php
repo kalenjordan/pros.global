1431ebed37cfbdb3c5f68f02ed3d3864726e753c
Kalen Jordan <kalen@magemail.co>
2019-02-22 07:09:52 -0600

    Sitemap should grab frontend app url

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -48,7 +48,7 @@ class GenerateSitemap extends Command
     {
         $this->info("Generating sitemap");
         $sitemap = Sitemap::create();
-        $sitemap->add(Url::create('/')
+        $sitemap->add(Url::create(env('FRONTEND_APP_URL') . '/')
             ->setLastModificationDate(Date::yesterday())
             ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
             ->setPriority(0.1));
@@ -56,7 +56,7 @@ class GenerateSitemap extends Command
         $users = User::all();
         $this->info($users->count() . " users");
         foreach ($users as $user) {
-            $url = Url::create('/' . $user->username);
+            $url = Url::create(env('FRONTEND_APP_URL') . '/' . $user->username);
             if ($user->updated_at) {
                 $url->setLastModificationDate($user->updated_at);
             }
@@ -68,7 +68,7 @@ class GenerateSitemap extends Command
         $this->info($savedSearches->count() . " saved searches");
         foreach ($savedSearches as $savedSearch) {
             /** @var SavedSearch $savedSearch */
-            $url = Url::create('/s/' . $savedSearch->getSlugOrId());
+            $url = Url::create(env('FRONTEND_APP_URL') . '/s/' . $savedSearch->getSlugOrId());
             if ($savedSearch->updated_at) {
                 $url->setLastModificationDate($savedSearch->updated_at);
             }
@@ -80,7 +80,7 @@ class GenerateSitemap extends Command
         $this->info($tags->count() . " tags");
         foreach ($tags as $tag) {
             /** @var Tag $tag */
-            $url = Url::create('/tag/' . $tag->slug);
+            $url = Url::create(env('FRONTEND_APP_URL') . '/tag/' . $tag->slug);
             if ($tag->updated_at) {
                 $url->setLastModificationDate($tag->updated_at);
             }
