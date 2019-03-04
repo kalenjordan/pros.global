db0f433251f7f7e37f7895d31aacf96d2daa78da
Kalen Jordan <kalen@magemail.co>
2019-03-04 09:17:55 -0600

    Update unread notification email to include link to message

--- resources/views/emails/unread-notifications.blade.php
+++ resources/views/emails/unread-notifications.blade.php
@@ -7,7 +7,11 @@ $count = $user->notificationsToEmail()->count();
 # You have {{ $count }} unread notification{{ $count > 1 ? "s" : "" }}
 
 @foreach ($user->notificationsToEmail()->orderby('created_at', 'desc')->get() as $notification)
-- {{ $notification->data['text'] }} - {{ \App\Date::parse($notification->created_at)->diffForHumans() }}
+ - {{ $notification->data['text'] }}
+  @if (isset($notification->data['link']) && isset($notification->data['link']['cta']) && isset($notification->data['link']['link']))
+   [{{ $notification->data['link']['cta'] }}]({{ $notification->data['link']['link'] }})
+  @endif
+ ({{ \App\Date::parse($notification->created_at)->diffForHumans() }})
 @endforeach
 
 @component('mail::button', ['url' => $user->url()])
